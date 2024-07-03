4759d420c95e181ad7ad8370cbd0a9309c041088
tinashe.changunda <tinashe.changunda@workday.com>
2024-07-03 19:59:14 +0100

    added category and addition function to API

--- src/main/java/com/tinashe/quizapp/controller/QuestionController.java
+++ src/main/java/com/tinashe/quizapp/controller/QuestionController.java
@@ -1,11 +1,10 @@
 package com.tinashe.quizapp.controller;
 
 import com.tinashe.quizapp.Question;
+import com.tinashe.quizapp.dao.QuestionDao;
 import com.tinashe.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,10 +13,23 @@ import java.util.List;
 public class QuestionController {
 
     @Autowired
+    //QuestionDao questionDao;
     QuestionService questionService;
 
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    //allows us to get questions by category and store them
+    //we'll pass that categorical question through the method below as a PathVariable
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
     }
 }
